cc3c94979f9d590e51966ccb7cf7382aa468e48b
REN <145991196+RenArslan@users.noreply.github.com>
2025-11-16 11:41:22 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
 export default defineConfig(({ mode }) => ({
-  base: "/responsive-kumite-site/", // IMPORTANT
+  base: "/responsive-kumite-site/",
   server: {
     host: "::",
     port: 8080,
