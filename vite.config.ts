91d7398fdcd37deca9e7d8fac1c69d0c9fd5a42c
REN <145991196+RenArslan@users.noreply.github.com>
2025-11-16 12:38:07 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "node:path";
+import path from "path";
 
 export default defineConfig({
-  base: "/responsive-kumite-site/",   // correct public base path
+  base: "/",   // IMPORTANT for custom domain
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
