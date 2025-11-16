05a513a0126660264c4b042b9ce8f2f74f302d36
REN <145991196+RenArslan@users.noreply.github.com>
2025-11-16 11:46:36 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
-  base: "/", // required for custom domain + GitHub Pages with CNAME
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+export default defineConfig({
+  base: "/responsive-kumite-site/",   // IMPORTANT for GitHub Pages
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
